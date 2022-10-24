b6a5b2942d664732b6a05331c1783aceb5a6baf0
Juacy Willian <juacy.willian@gmail.com>
2022-10-24 10:32:49 -0300

    kata 002

---
+++ kata_002.py
@@ -0,0 +1,23 @@
+# Make a function that will return a greeting statement that uses an input; 
+# your program should return, "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+
+# assert_equals(greet('Ryan'), "Hello, Ryan how are you doing today?")
+# assert_equals(greet('Shingles'), "Hello, Shingles how are you doing today?")
+import unittest
+
+class TestZeroFuelFunction(unittest.TestCase):
+
+    def test_greetings(self, ):
+        self.assertEqual(greet('Ryan'), "Hello, Ryan how are you doing today?", "Passed")
+        self.assertEqual(greet('Shingles'), "Hello, Shingles how are you doing today?", "Passed")
+        
+
+if __name__ == '__main__':
+    unittest.main()
