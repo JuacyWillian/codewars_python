8f920a22abea26695b0748b28dad9f44b479f250
Juacy Willian <juacy.willian@gmail.com>
2022-10-24 10:35:49 -0300

    comentando os desafios

--- kata_001.py
+++ kata_001.py
@@ -10,11 +10,13 @@
 # Function should return true if it is possible and false if not.
 
 
-
+# Challenge
 def zero_fuel(distance_to_pump, mpg, fuel_left):
     return True if distance_to_pump<=(mpg*fuel_left) else False
 
 
+
+# Tests
 import unittest
 
 class TestZeroFuelFunction(unittest.TestCase):
--- kata_002.py
+++ kata_002.py
@@ -4,12 +4,13 @@
 # [Make sure you type the exact thing I wrote or the program may not execute properly]
 
 
+# Challenge
 def greet(name):
     return f"Hello, {name} how are you doing today?"
 
 
-# assert_equals(greet('Ryan'), "Hello, Ryan how are you doing today?")
-# assert_equals(greet('Shingles'), "Hello, Shingles how are you doing today?")
+
+# Tests
 import unittest
 
 class TestZeroFuelFunction(unittest.TestCase):
@@ -17,7 +18,7 @@ class TestZeroFuelFunction(unittest.TestCase):
     def test_greetings(self, ):
         self.assertEqual(greet('Ryan'), "Hello, Ryan how are you doing today?", "Passed")
         self.assertEqual(greet('Shingles'), "Hello, Shingles how are you doing today?", "Passed")
-        
+
 
 if __name__ == '__main__':
     unittest.main()
